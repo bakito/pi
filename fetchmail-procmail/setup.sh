a5e4e3c78d2fd90d75534382e47d0aa61b97bd4d
tkb16 <marc.brugger@six-group.com>
2015-02-02 10:45:10 +0100

    parse values

--- fetchmail-procmail/setup.sh
+++ fetchmail-procmail/setup.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 
+function read_value() 
+{
+    while [[ -z "$NEW_VALUE" ]] ; do
+		read NEW_VALUE
+	done
+    echo $NEW_VALUE
+}
+
 echo "Enter the Server name"
-read SERVER_NAME
-echo "Server Name = ${SERVER_NAME}"
+SERVER_NAME=$(read_value)
 
 echo "Enter the Server URL"
-read SERVER_URL
-echo "Server URL = ${SERVER_URL}"
+SERVER_URL=$(read_value)
 
 echo "Enter the User Name"
-read USER_NAME
-echo "User Name = ${USER_NAME}"
+USER_NAME=$(read_value)
 
 
-echo -n "Enter the User Password"
-read -s USER_PASSWORD
-echo
-# Run Command
-echo "User Password = $USER_PASSWORD"
+while [[ -z "$USER_PASSWORD" ]] ; do
+	echo -n "Enter the User Password"
+    echo
+	read -s USER_PASSWORD
+done
+
+
+
 
 echo "Enter the Server Fetch Interval in Seconds [900]"
-read SERVER_FETCH_INTERVAL
 echo "Server Fetch Interval = ${SERVER_FETCH_INTERVAL}"
+while [[ -z "$SERVER_FETCH_INTERVAL" ]] ||  [[ ! $SERVER_FETCH_INTERVAL =~ ^-?[0-9]+$ ]]; do
+	SERVER_FETCH_INTERVAL=$(read_value)
+done
 
+echo "Server Fetch Interval = ${SERVER_FETCH_INTERVAL}"
